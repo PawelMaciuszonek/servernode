b50f19cdd920628d2c515b1eedbaf120d2761d94
Paweł Maciuszonek <superpavcio6@gmail.com>
2025-09-25 08:26:18 +0200

    Opis zmian

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  if (req.method === 'GET') {
+    // Pokazuje formularz
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(`
+      <form method="POST">
+        <label>Podaj imię:</label>
+        <input type="text" name="imie" />
+        <button type="submit">Wyślij</button>
+      </form>
+    `);
+  } else if (req.method === 'POST') {
+    // Odbiera dane z formularza
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const imie = new URLSearchParams(body).get('imie');
+      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+      res.end(`Cześć, ${imie}!`);
+    });
+  }
 });
 
 server.listen(3000, () => {
-  console.log('Serwer działa na porcie 3000');
+  console.log('Serwer działa na http://localhost:3000');
 });
